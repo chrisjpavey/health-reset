810768bbc607029d2ea90c8e999d4502ee4a8017
chrisjpavey <chris.jpavey@googlemail.com>
2025-10-29 09:28:24 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'health-reset-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
